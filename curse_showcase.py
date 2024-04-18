99908b78e09de7f9519ff9d7b3e5a5e00ea5bf0b
Ceribrox <caleb.j.armour@gmail.com>
2024-04-18 16:57:07 -0600

    Testing the ability to push changes from VScode

--- curse_showcase.py
+++ curse_showcase.py
@@ -3,6 +3,8 @@ import time
 import os
 from curses.textpad import Textbox, rectangle
 
+#test change 
+
 def main(stdscr):
     # Setting color pairs
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
